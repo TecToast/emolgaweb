a3f68c5a455961b3962606883caf7d1eb00be514
Flo <tectoast@gmail.com>
2024-04-08 12:22:22 +0200

    refactor: rewrite text on landing page

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,15 @@
 
 <template>
   <div class="mt-5 flex flex-row justify-center">
-    <p class="text-xl text-gray-300 max-w-5xl">Hello there, ich bin ein Discord-Bot, der von <code>{{
+    <p class="text-xl text-gray-300 max-w-5xl">Dies ist die Website von Emolga, einem Discord-Bot, der von <code>{{
         MY_DISCORD_TAG
       }}</code> (Flo | TecToast) programmiert wurde und hauptsächlich auf Pokémon spezialisiert ist. Hauptaufgabe ist
-      die automatische Analyse von Pokemon Showdown Replays. Bei Fragen bitte an Flo wenden :) Falls ihr Feedback für
-      mich habt, schreibt mir oder Flo eine PN!</p>
+      die automatische Analyse von <NuxtLink to="https://play.pokemonshowdown.com/" class="underline">Pokemon Showdown!</NuxtLink> Replays. Bei Fragen/Problemen/Feedback können Sie gerne auf meinen <NuxtLink to="https://discord.gg/WYfKHPCgs9"><UIcon name="i-logos-discord-icon" dynamic/> <span class="underline">Support-Server</span></NuxtLink> kommen.</p>
   </div>
+
   <div class="mt-5 flex flex-row justify-center">
-    <p class="text-xl text-gray-300 max-w-5xl">Diese Seite hier befindet sich noch im Aufbau, später wird es eventuell
-      ein Dashboard geben :)</p>
+    <p class="text-xl text-gray-300 max-w-5xl"><UIcon name="i-mdi-hammer-wrench" dynamic/> Diese Seite hier befindet sich noch im Aufbau, später wird es eventuell
+      ein Dashboard geben <UIcon name="i-mdi-hammer-wrench" dynamic/></p>
   </div>
 </template>
 
