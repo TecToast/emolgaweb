64069cdcb3881390f7c09a5888f0d27d2e90bea3
Flo <tectoast@gmail.com>
2024-04-08 12:27:04 +0200

    feat: add route rule for api proxy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
+  routeRules: {
+    "/api/**": {
+      proxy: "http://localhost:58700/api/**"
+    }
+  },
   devtools: {
     enabled: true,
     timeline: {
