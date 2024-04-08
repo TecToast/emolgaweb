18772d1afeeaa6c2e014c34991f8f1b6c8d156d4
Flo <tectoast@gmail.com>
2024-04-08 12:19:46 +0200

    fix: footer doesn't overlap with UI on small screens

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <template>
-  <Header/>
-  <div class="mx-2 lg:mx-0">
-    <NuxtPage/>
+  <div class="flex flex-col" style="min-height: 100vh">
+    <Header/>
+    <div class="mx-2 xl:mx-0">
+      <NuxtPage/>
+    </div>
+    <Footer/>
   </div>
-  <Footer/>
 </template>
 <script setup lang="ts">
 </script>
--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <footer class="text-white text-center text-xs p-3 fixed bottom-0 w-full z-50">
+  <footer class="text-white text-center text-xs p-3 mt-auto w-full z-50">
     <NuxtLink to="/de/tos" class="border-r pr-4">Terms of Service</NuxtLink>
     <NuxtLink to="/de/privacy" class="pl-4">Privacy Policy</NuxtLink>
   </footer>
