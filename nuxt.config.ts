e2ca205ad638d87046c634651007e11f36f46f0d
Flo <tectoast@gmail.com>
2024-04-21 22:39:16 +0200

    refactor: better tunnel settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const tunnel = false;
+const tunnelSettings = {
+    localtunnel: {
+        subdomain: "emolga"
+    }
+}
 export default defineNuxtConfig({
-  ssr: true,
-  routeRules: {
-    "/api/**": {
-      proxy: "http://localhost:58700/api/**"
-    }
-  },
-  devtools: {
-    enabled: true,
-    timeline: {
-      enabled: true
-    }
-  },
-  modules: ["@nuxt/ui", "@nuxt/image", "@pinia/nuxt", /*"@nuxtus/nuxt-localtunnel"*/],
-  app: {
-    head: {
-      title: "Emolga"
-    }
-  },
-  imports: {
-    presets: [
-      {
-        from: "vuedraggable",
-        imports: ["draggable"]
-      }
-    ]
-  }
-  /*localtunnel: {
-    subdomain: "emolga"
-  }*/
+    ssr: true,
+    routeRules: {
+        "/api/**": {
+            proxy: "http://localhost:58700/api/**"
+        }
+    },
+    devtools: {
+        enabled: true,
+        timeline: {
+            enabled: true
+        }
+    },
+    modules: ["@nuxt/ui", "@nuxt/image", "@pinia/nuxt", ...(tunnel ? ["@nuxtus/nuxt-localtunnel"] : [""])],
+    app: {
+        head: {
+            title: "Emolga"
+        }
+    },
+    imports: {
+        presets: [
+            {
+                from: "vuedraggable",
+                imports: ["draggable"]
+            }
+        ]
+    },
+    ...tunnelSettings
 })
