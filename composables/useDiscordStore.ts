14e413c93d15702182a575b9c96d167bc94453ed
Flo <tectoast@gmail.com>
2024-09-13 12:38:20 +0200

    refactor: disable api calls for now

--- composables/useDiscordStore.ts
+++ composables/useDiscordStore.ts
@@ -1,14 +1,23 @@
 import type {League, UserData} from "~/utils/types";
 
 export const useDiscordStore = defineStore('discord', () => {
-    const {data: user, execute: loadUser, error, pending, status } = useFetch<UserData>('/api/userdata', {timeout: 2000, credentials: 'include'})
+    /*const {data: user, execute: loadUser, error, pending, status } = useFetch<UserData>('/api/userdata', {timeout: 2000, credentials: 'include'})
     const {data: leagues, execute: loadLeagues} = useFetch<League[]>('/api/manage/myleagues', {
         timeout: 2000,
         credentials: 'include',
         immediate: false
-    })
+    })*/
+    const user = ref<UserData | null>(null)
+    const leagues = ref<League[] | null>(null)
+    function loadUser() {
+        // ...
+    }
+    function loadLeagues() {
+        // ...
+    }
     function shouldRequestUser() {
-        return status.value == "idle"
+        // return status.value == "idle"
+        return false
     }
     return {
         user,
