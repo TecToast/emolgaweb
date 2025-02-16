8c3d6533a1059c7376a9669fbf78ebef750aa831
Flo <tectoast@gmail.com>
2025-02-16 01:56:00 +0100

    feat: prepare for Nuxt v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
+  future: {
+    compatibilityVersion: 4,
+  },
   routeRules: {
     "/api/**": {
-      proxy: "http://localhost:58700/api/**"
-    }
+      proxy: "http://localhost:58700/api/**",
+    },
   },
   devtools: {
     enabled: true,
     timeline: {
-      enabled: true
-    }
+      enabled: true,
+    },
   },
   modules: ["@nuxt/ui", "@nuxt/image", "@pinia/nuxt"],
   app: {
     head: {
-      title: "Emolga"
-    }
-  }
-})
+      title: "Emolga",
+    },
+  },
+});
