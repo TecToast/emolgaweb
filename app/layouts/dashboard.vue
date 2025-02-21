80376d8cb4d07ff6f8cc1978f0688b1f6cc9aab9
Flo <tectoast@gmail.com>
2025-02-21 17:16:21 +0100

    refactor: remove cookie policy from template

--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -101,35 +101,6 @@ const linksInServer = computed(() => {
     },
   ];
 });
-
-onMounted(async () => {
-  const cookie = useCookie("cookie-consent");
-  if (cookie.value === "accepted") {
-    return;
-  }
-
-  toast.add({
-    title:
-      "We use first-party cookies to enhance your experience on our website.",
-    duration: 0,
-    close: false,
-    actions: [
-      {
-        label: "Accept",
-        color: "neutral",
-        variant: "outline",
-        onClick: () => {
-          cookie.value = "accepted";
-        },
-      },
-      {
-        label: "Opt out",
-        color: "neutral",
-        variant: "ghost",
-      },
-    ],
-  });
-});
 </script>
 
 <template>
