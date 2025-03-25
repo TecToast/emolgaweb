aff6234355a23d394daa53d2792d9b2f7e4191ce
Flo <tectoast@gmail.com>
2025-03-25 20:45:04 +0100

    feat(config): experiments

--- app/components/EmolgaConfig.vue
+++ app/components/EmolgaConfig.vue
@@ -6,11 +6,13 @@ const { structure, initialContent } = defineProps<{
   initialContent: any;
 }>();
 const content = reactive(initialContent);
+const changedContent = reactive({});
 const route = useRoute();
 const path = route.params.path;
 const resolvedPath = (
   typeof path === "string" ? (path === "" ? [] : [path]) : path
 )!;
+provide("changedContent", changedContent);
 const finishedPath: BreadcrumbItem[] = [structure.name, ...resolvedPath].map(
   (item, index) => ({
     label: item,
@@ -23,52 +25,27 @@ const finishedPath: BreadcrumbItem[] = [structure.name, ...resolvedPath].map(
 let currContent = content;
 let currStructure: ConfigValue = structure;
 for (const p of resolvedPath) {
-  if (currStructure.type === "CLASS") {
-    const nextStructure = currStructure.value[p];
-    if (!nextStructure) {
-      break;
-    }
-    currContent = currContent[p];
-    currStructure = nextStructure;
-  } else if (currStructure.type === "LIST") {
-    const nextData = currStructure.value[0];
-    if (!nextData) {
-      break;
-    }
-    currContent = currContent[p];
-    currStructure = nextData;
-  } else if (currStructure.type === "MAP") {
-    const nextData = currStructure.value[1];
-    if (!nextData) {
-      break;
-    }
-    currContent = currContent[p];
-    currStructure = nextData;
+  const nextData: ConfigValue | undefined =
+    currStructure.type === "CLASS"
+      ? currStructure.value[p]
+      : currStructure.type === "LIST"
+      ? currStructure.value[0]
+      : currStructure.type === "MAP"
+      ? currStructure.value[1]
+      : undefined;
+  if (!nextData) {
+    break;
   }
+  currContent = currContent[p];
+  currStructure = nextData;
 }
-
-/*function parseTree(tree: ConfigObject): TreeItem[] {
-  return Object.entries(tree).map(([key, value]) => {
-    if (value.type === "CLASS") {
-      return { label: value.name, children: parseTree(value.value) };
-    }
-    if (value.type === "SEALED") {
-      return {
-        label: value.name ?? key,
-        children: parseTree(value.value.value.value),
-      };
-    }
-    return { label: value.name ?? key };
-    // if ([].includes(value.type)) {}
-  });
-}
-const tree = ref(parseTree(data));*/
 </script>
 
 <template>
   <div class="w-full lg:max-w-4xl mx-auto">
     <div>
       <UBreadcrumb :items="finishedPath" class="mb-4" />
+      {{ changedContent }}
       <EmolgaConfigPart v-model="currContent" :data="currStructure" />
     </div>
   </div>
