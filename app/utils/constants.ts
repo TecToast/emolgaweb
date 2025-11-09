bf08a57789c7870bad9843f01ac729e0c8bfb733
Flo <tectoast@gmail.com>
2025-11-09 16:58:20 +0100

    fix: new server invite url

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -3,7 +3,7 @@ export const ALPHA = true;
 export const MY_DISCORD_TAG = "@tectoast";
 export const BOT_INVITE_URL =
   "https://discord.com/api/oauth2/authorize?client_id=723829878755164202&permissions=3072&scope=bot";
-export const DISCORD_SERVER_INVITE_URL = "https://discord.gg/WYfKHPCgs9";
+export const DISCORD_SERVER_INVITE_URL = "https://discord.gg/zakJ4p4mUf";
 
 export function convertLeagueIDToName(leagueid: string) {
   return leagueid.replace(/(.+)S(\d+)L?(\d+)?/g, "$1 Saison $2 Liga $3");
