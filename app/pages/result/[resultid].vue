fdea378b5dbbe817f5539edf192d62734ef13802
Flo <tectoast@gmail.com>
2025-12-14 14:11:45 +0100

    fix: adapt to backend changes

--- app/pages/result/[resultid].vue
+++ app/pages/result/[resultid].vue
@@ -1,18 +1,20 @@
 <script lang="ts" setup>
-import type {ResultCodeResponse} from "~/utils/types";
+import type { ResultCodeResponse } from "~/utils/types";
 
 const route = useRoute();
 const router = useRouter();
 const toast = useToast();
 const resultid = route.params.resultid;
-const {data, error} = await useFetch<ResultCodeResponse>(
-    `/api/emolga/result/${resultid}`
+const { data, error } = await useFetch<ResultCodeResponse>(
+  `/api/emolga/result/${resultid}`
 );
-type PlayerData = { [mon: string]: { k: number; d: boolean } };
+type PlayerData = { [mon: string]: { k: number; d: number } };
 type ResultData = Record<string, PlayerData>;
 const allResultDatas = ref<ResultData[]>([{}, {}, {}]);
 const selectedGame = ref(1);
-const resultData = computed(() => allResultDatas.value[selectedGame.value - 1]!)
+const resultData = computed(
+  () => allResultDatas.value[selectedGame.value - 1]!
+);
 const submitting = ref(false);
 
 function togglePokemonSelection(playerName: string, pokemon: string) {
@@ -28,31 +30,44 @@ function togglePokemonSelection(playerName: string, pokemon: string) {
       delete resultData.value[playerName];
     }
   } else {
-    playerData[pokemon] = {k: 0, d: false};
+    playerData[pokemon] = { k: 0, d: 0 };
   }
 }
 
 const invalidIndexes = computed(() => {
-  const invalidIndexes = []
+  const invalidIndexes = [];
   for (const [index, rsData] of Object.entries(allResultDatas.value)) {
     const keys = Object.keys(rsData);
-    if (!(index == "2" && keys.length == 0) && (keys.length != 2 || ![0, 1].every((i) => {
-      const playerData: PlayerData = rsData[keys[i] as string]!;
-      const opponentData: PlayerData = rsData[keys[1 - i] as string]!;
-      return (
-          Object.values(playerData).reduce((sum, mon) => sum + mon.k, 0) ==
-          Object.values(opponentData).reduce((sum, mon) => sum + (mon.d ? 1 : 0), 0)
-      );
-    }))) {
+    if (
+      !(index == "2" && keys.length == 0) &&
+      (keys.length != 2 ||
+        ![0, 1].every((i) => {
+          const playerData: PlayerData = rsData[keys[i] as string]!;
+          const opponentData: PlayerData = rsData[keys[1 - i] as string]!;
+          return (
+            Object.values(playerData).reduce((sum, mon) => sum + mon.k, 0) ==
+            Object.values(opponentData).reduce(
+              (sum, mon) => sum + (mon.d ? 1 : 0),
+              0
+            )
+          );
+        }))
+    ) {
       invalidIndexes.push(index);
     }
-    if(!data.value?.bo3) break;
+    if (!data.value?.bo3) break;
   }
   return invalidIndexes;
 });
 const errorMsg = computed(() => {
   if (invalidIndexes.value.length) {
-    return `Die Ergebnisse ${data.value?.bo3 ? `im Kampf ${invalidIndexes.value.map(itm => Number.parseInt(itm) + 1).join(' & ')} ` : ''}scheinen nicht korrekt zu sein. Bitte überprüfe die Eingaben.`;
+    return `Die Ergebnisse ${
+      data.value?.bo3
+        ? `im Kampf ${invalidIndexes.value
+            .map((itm) => Number.parseInt(itm) + 1)
+            .join(" & ")} `
+        : ""
+    }scheinen nicht korrekt zu sein. Bitte überprüfe die Eingaben.`;
   }
   return null;
 });
@@ -62,16 +77,20 @@ const modalDescription = computed(() => {
     return error;
   }
   return (
-      "Bist du sicher, dass du die Ergebnisse so eingeben möchtest?\n\n" +
-      allResultDatas.value.filter(rsData => Object.keys(rsData).length > 0).map(rsData =>
-          Object.entries(rsData).map(
-              ([player, mons]) =>
-                  `${player}:\n${Object.entries(mons)
-                      .map(([mon, data]) => `${mon} ${data.k} ${data.d ? "X" : ""}`)
-                      .join("\n")}`
+    "Bist du sicher, dass du die Ergebnisse so eingeben möchtest?\n\n" +
+    allResultDatas.value
+      .filter((rsData) => Object.keys(rsData).length > 0)
+      .map((rsData) =>
+        Object.entries(rsData)
+          .map(
+            ([player, mons]) =>
+              `${player}:\n${Object.entries(mons)
+                .map(([mon, data]) => `${mon} ${data.k} ${data.d ? "X" : ""}`)
+                .join("\n")}`
           )
-              .join("\n\n")).join("\n\n===============\n\n")
-
+          .join("\n\n")
+      )
+      .join("\n\n===============\n\n")
   );
 });
 
@@ -92,83 +111,89 @@ function sendResult() {
     method: "POST",
     body: JSON.stringify(payloads),
   })
-      .then(() => {
-        toast.add({
-          title: "Ergebnisse erfolgreich eingereicht!",
-          color: "success",
-        });
-        submitting.value = false;
-        router.push("/");
-      })
-      .catch((error) => {
-        toast.add({
-          title: "Fehler beim Einreichen der Ergebnisse!",
-          description: error.message || "Unbekannter Fehler",
-          color: "error",
-        });
-        submitting.value = false;
+    .then(() => {
+      toast.add({
+        title: "Ergebnisse erfolgreich eingereicht!",
+        color: "success",
       });
+      submitting.value = false;
+      router.push("/");
+    })
+    .catch((error) => {
+      toast.add({
+        title: "Fehler beim Einreichen der Ergebnisse!",
+        description: error.message || "Unbekannter Fehler",
+        color: "error",
+      });
+      submitting.value = false;
+    });
 }
 </script>
 
 <template>
   <div v-if="error" class="flex justify-center mt-4">
     <UAlert
-        title="Fehler beim Laden der Ergebniseingabe"
-        :description="`Wenn du denkst, dass der Fehler nicht bei dir liegt, melde dich bitte bei ${MY_DISCORD_TAG}.`"
-        color="error"
-        variant="subtle"
-        class="w-1/2"
+      title="Fehler beim Laden der Ergebniseingabe"
+      :description="`Wenn du denkst, dass der Fehler nicht bei dir liegt, melde dich bitte bei ${MY_DISCORD_TAG}.`"
+      color="error"
+      variant="subtle"
+      class="w-1/2"
     />
   </div>
   <div v-if="data" class="flex flex-col items-center my-4 gap-8">
     <div class="flex flex-col items-center gap-2">
       <img
-          v-if="data.logoUrl"
-          :src="data.logoUrl"
-          alt="Guild Logo"
-          class="w-16 h-16 rounded-full"
+        v-if="data.logoUrl"
+        :src="data.logoUrl"
+        alt="Guild Logo"
+        class="w-16 h-16 rounded-full"
       />
       <h2 class="text-lg font-semibold">{{ data.guildName }}</h2>
       <h3 class="text-md">Spieltag {{ data.gameday }}</h3>
       <ConfirmModal
-          title="Ergebniseingabe bestätigen"
-          :description="modalDescription"
-          btn-color="success"
-          btn-icon="i-heroicons-check"
-          btn-label="Bestätigen"
-          :disabled="!!invalidIndexes.length"
-          :error-msg="errorMsg"
-          :on-confirm="sendResult"
-          :loading-confirm="submitting"
+        title="Ergebniseingabe bestätigen"
+        :description="modalDescription"
+        btn-color="success"
+        btn-icon="i-heroicons-check"
+        btn-label="Bestätigen"
+        :disabled="!!invalidIndexes.length"
+        :error-msg="errorMsg"
+        :on-confirm="sendResult"
+        :loading-confirm="submitting"
       />
       <div v-if="data.bo3" class="my-4">
-        <p class="px-2 xl:px-0 py-4">Wähle hier nacheinander die Kämpfe aus und trage die entsprechenden Daten ein. Lass
-          den dritten Kampf leer, falls es nur zwei Kämpfe gab.</p>
+        <p class="px-2 xl:px-0 py-4">
+          Wähle hier nacheinander die Kämpfe aus und trage die entsprechenden
+          Daten ein. Lass den dritten Kampf leer, falls es nur zwei Kämpfe gab.
+        </p>
         <UTabs
-            v-model="selectedGame"
-            :content="false"
-            :items="[1,2,3].map((itm) => ({label: `Kampf ${itm}`, value: itm}))"></UTabs>
+          v-model="selectedGame"
+          :content="false"
+          :items="
+            [1, 2, 3].map((itm) => ({ label: `Kampf ${itm}`, value: itm }))
+          "
+        ></UTabs>
       </div>
       <p class="px-2 xl:px-0">
         Klicke alle Pokémon an, die in diesem Kampf verwendet wurden, und stelle
-        dann jeweils die Kills und Deaths ein. Wenn du {{ data.bo3 ? 'mit allen Kämpfen ' : '' }}fertig bist, klicke auf
-        den Bestätigen-Button.
+        dann jeweils die Kills und Deaths ein. Wenn du
+        {{ data.bo3 ? "mit allen Kämpfen " : "" }}fertig bist, klicke auf den
+        Bestätigen-Button.
       </p>
     </div>
     <div
-        class="flex w-full flex-col xl:flex-row justify-around gap-16 xl:gap-0"
+      class="flex w-full flex-col xl:flex-row justify-around gap-16 xl:gap-0"
     >
       <div
-          v-for="pdata of data.data"
-          :key="pdata.name"
-          class="flex flex-col items-center gap-4"
+        v-for="pdata of data.data"
+        :key="pdata.name"
+        class="flex flex-col items-center gap-4"
       >
         <div class="flex gap-2 items-center">
           <img
-              :src="pdata.avatarUrl"
-              alt="Player Avatar"
-              class="w-8 h-8 rounded-full"
+            :src="pdata.avatarUrl"
+            alt="Player Avatar"
+            class="w-8 h-8 rounded-full"
           />
           <h4 class="text-md font-semibold">{{ pdata.name }}</h4>
         </div>
@@ -179,8 +204,8 @@ function sendResult() {
               <template v-for="mon of pdata.picks" :key="mon.tlName">
                 <div class="w-full flex items-center gap-4">
                   <div
-                      class="flex flex-col xl:flex-row items-center gap-2"
-                      :class="
+                    class="flex flex-col xl:flex-row items-center gap-2"
+                    :class="
                       resultData[pdata.name]?.[mon.tlName]
                         ? 'visible'
                         : 'invisible'
@@ -189,40 +214,38 @@ function sendResult() {
                     <p class="text-sm">Kills</p>
                     <ClientOnly>
                       <UInputNumber
-                          :default-value="0"
-                          :min="0"
-                          :max="6"
-                          :model-value="
-                        resultData[pdata.name]?.[mon.tlName]?.k ?? 0
-                      "
-                          @update:model-value="(value) => resultData[pdata.name]![mon.tlName]!.k = value"
+                        :default-value="0"
+                        :min="0"
+                        :max="6"
+                        :model-value="
+                          resultData[pdata.name]?.[mon.tlName]?.k ?? 0
+                        "
+                        @update:model-value="(value) => resultData[pdata.name]![mon.tlName]!.k = value"
                       />
                     </ClientOnly>
                   </div>
                   <div
-                      class="rounded-lg ring ring-default p-2 flex flex-col xl:flex-row gap-2 items-center cursor-pointer w-full"
-                      :class="{
+                    class="rounded-lg ring ring-default p-2 flex flex-col xl:flex-row gap-2 items-center cursor-pointer w-full"
+                    :class="{
                       'bg-primary-900': resultData[pdata.name]?.[mon.tlName],
                     }"
-                      @click="togglePokemonSelection(pdata.name, mon.tlName)"
+                    @click="togglePokemonSelection(pdata.name, mon.tlName)"
                   >
                     <img
-                        :src="`https://play.pokemonshowdown.com/sprites/gen5/${mon.spriteName}.png`"
+                      :src="`https://play.pokemonshowdown.com/sprites/gen5/${mon.spriteName}.png`"
                     />
                     <p>{{ mon.tlName }}</p>
                   </div>
                   <UCheckbox
-                      :class="
+                    :class="
                       resultData[pdata.name]?.[mon.tlName]
                         ? 'visible'
                         : 'invisible'
                     "
-                      size="xl"
-                      label="Besiegt?"
-                      :model-value="
-                      resultData[pdata.name]?.[mon.tlName]?.d ?? false
-                    "
-                      @update:model-value="(value) => resultData[pdata.name]![mon.tlName]!.d = !!value"
+                    size="xl"
+                    label="Besiegt?"
+                    :model-value="resultData[pdata.name]?.[mon.tlName]?.d == 1"
+                    @update:model-value="(value) => resultData[pdata.name]![mon.tlName]!.d = value ? 1 : 0"
                   />
                 </div>
               </template>
